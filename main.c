ddc23630a805d8f0032ba56e2b413bfdadbdf582
J.G <jules.guillot@etu.univ-savoie.fr>
2024-10-02 11:53:00 +0200

    A verifier avec la carte nucleo

--- main.c
+++ main.c
@@ -3,10 +3,13 @@
 int main(void)
 {
 	// Enable Clock GPIOA 	(RCC->AHB1ENR)
+	RCC->AHB1ENR |= 1;
 	// PA5 Output			(GPIOA->MODER)
+	uint32_t * ptr_GPIOA_ODR = (uint32_t *) 0x40020014;
 
 	while(1){
 		for(uint32_t i = 0 ; i<100000; i++);
-		// Toggle PA5		(GPIOA->ODR)	
+		// Toggle PA5		(GPIOA->ODR)
+		(*ptr_GPIOA_ODR) ^= (1<<5);
 	}
 }
